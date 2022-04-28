51a493de622903c73e242ee55ac31feaf2886603
fadhilahm <fadhilahmetra@gmail.com>
2022-04-28 18:33:24 +0900

    Finished until chapter 8.

---
+++ shellscript.sh/escape.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "A quote is \", backslash is \\, backtick is \`."
+echo "A few spaces are    and dollar is \$. \$X is 5."
+
---
+++ shellscript.sh/myvar2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "MYVAR IS: $MYVAR"
+MYVAR="hi there"
+echo "MYVAR IS: $MYVAR"
+
